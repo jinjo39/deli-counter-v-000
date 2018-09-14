4f13dfe5ff586e3a7a15b2a5b797cf4542f638c7
Christina Delavar <c.delavar@gmail.com>
2018-09-14 18:48:55 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def line(array)
+  if array.count == 0
+    puts "The line is currently empty."
+  else
+    string = "The line is currently: "
+    array.map.each_with_index do |person, index|
+      string << "#{index+1}. #{person}"
+
+end
